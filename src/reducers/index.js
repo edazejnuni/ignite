81c9565023e94a94c852f4515297e830dde669c5
edazejnuni <edona.zejnuni@gmail.com>
2021-11-09 15:58:27 +0100

    2.API Setup

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import gamesReducer from './gamesReducers';
+
+const rootReducer = combineReducers({
+    games: gamesReducer,
+});
+
+export default rootReducer;
+ 
